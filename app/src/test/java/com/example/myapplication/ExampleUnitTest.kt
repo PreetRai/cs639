2ddfe733ea2a7ba683bc70e64f0df5914f358b9c
PreetRai <prithviraimangya@gmail.com>
2023-10-09 15:29:13 -0400

    Random number app - run test and add 3 logs

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,17 +1,51 @@
 package com.example.myapplication
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testCase1() {
+        // Test Case 1
+        val result = add(2, 3)
+        println("Test Case 1: Adding 2 and 3")
+        println("Result: $result")
+        assert(result == 5)
+    }
+
+    @Test
+    fun testCase2() {
+        // Test Case 2
+        val result = subtract(5, 3)
+        println("Test Case 2: Subtracting 3 from 5")
+        println("Result: $result")
+        assert(result == 2)
+    }
+
+    @Test
+    fun testCase3() {
+        // Test Case 3
+        val text = concatenate("Hello, ", "world!")
+        println("Test Case 3: Concatenating strings")
+        println("Result: $text")
+        assert(text == "Hello, world!")
+    }
+
+    private fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    private fun subtract(a: Int, b: Int): Int {
+        return a - b
+    }
+
+    private fun concatenate(a: String, b: String): String {
+        return a + b
     }
 }
